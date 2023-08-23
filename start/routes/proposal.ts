a234ae2a548ee112fe44a0de902519139e51a7b1
Rafael <jiyuunotameni@hotmail.com>
2023-08-23 16:47:49 -0300

    refactor: update proposals index

--- start/routes/proposal.ts
+++ start/routes/proposal.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 export default function routes(){
   Route.group(() => {
-    Route.resource('/proposal', 'ProposalsController').apiOnly()
+    Route.resource('/proposal', 'ProposalsController').apiOnly().except(['index'])
+    Route.get('/proposals-by-prompt/:id', 'ProposalsController.indexByPrompt')
   }).middleware('auth')
 }
