2c6817d137f4da750178545109d61626a1bddb52
Rafael <jiyuunotameni@hotmail.com>
2023-09-03 12:09:33 -0300

    refactor: oh my god

--- app/Validators/PromptValidator.ts
+++ app/Validators/PromptValidator.ts
@@ -5,8 +5,10 @@ import { MyValidator } from './MyValidator'
 const PromptValidatorSchema = schema.create({
   title: schema.string(),
   text: schema.string(),
-  maxSizePerExtension: schema.number(),
-  limitOfExtensions: schema.number(),
+  concluded: schema.boolean.optional(),
+  maxSizePerExtension: schema.number([rules.unsigned()]),
+  limitOfExtensions: schema.number([rules.unsigned()]),
+  timeForAvanceInMinutes: schema.number([rules.unsigned()]),
   genreIds: schema.array([rules.minLength(1)]).members(schema.number()),
 })
 
