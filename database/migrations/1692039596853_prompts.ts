b30d8f45dfaf3051222b79dd0dd2a0ec1d9c7a5b
Rafael <jiyuunotameni@hotmail.com>
2023-09-04 13:29:43 -0300

    refactor: update migrations

--- database/migrations/1692039596853_prompts.ts
+++ database/migrations/1692039596853_prompts.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('write_id').unsigned().references('writes.id')
+      table.integer('write_id').unsigned().references('writes.id').onDelete('CASCADE')
 
       table.string('title').notNullable()
       table.integer('current_index').defaultTo(0)
--- database/migrations/1692197150250_proposals.ts
+++ database/migrations/1692197150250_proposals.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('write_id').unsigned().references('writes.id')
+      table.integer('write_id').unsigned().references('writes.id').onDelete('CASCADE')
       table.integer('prompt_id').unsigned().references('prompts.id')
       table.integer('order_in_history').unsigned().notNullable()
       table.boolean('definitive').defaultTo(false)
